7726ce9370dd86c67a1f06368f638c8a80d4357c
Rosesandthorns <powwerofpowwer@gmail.com>
2025-02-18 11:28:57 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+// data.js
+const pokemonList = [
+    {
+        nickname: "Chop",
+        species: "#475 - Gallade",
+        description: "This is Chop, probably my most used pokemon ever. Chop was caught in SV and has been my false swiper ever since, and runs heal pulse, false swipe, hypnosis, and psychic, and [...]",
+        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Gallade.webp",
+        tags: ["Favourite", "Fighting", "Psychic", "SV"]
+    },
+    // ... (rest of the Pokémon data)
+];
+
+const originalPokemonList = [...pokemonList]; // Store original list
