90aefaf31d99efda24b8036cfaf9048065fbbd87
Rosesandthorns <powwerofpowwer@gmail.com>
2025-03-03 08:55:44 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,283 +1,283 @@
 // data.js
 const pokemonList = [
     {
-    nickname: "Chop",
-    species: "#475 - Gallade",
-    description: "This is Chop, probably my most used pokemon ever. Chop was caught in SV and has been my false swiper ever since, and runs heal pulse, false swipe, hypnosis, and psychic, and has the abiltiy Sharpness to make false swipe stronger.",
-    imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/2/2d/Gallade.png",
-    tags: ["Favourite", "Fighting", "Psychic", "SV"]
+        nickname: "Chop",
+        species: "#475 - Gallade",
+        description: "This is Chop, probably my most used pokemon ever. Chop was caught in SV and has been my false swiper ever since, and runs heal pulse, false swipe, hypnosis, and psychic, and has the abiltiy Sharpness to make false swipe stronger.",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/2/2d/Gallade.png",
+        tags: ["Favourite", "Fighting", "Psychic", "SV"]
     },
     {
         nickname: "Chedder",
         species: "#0805 - Stakataka",
         description: "This Yellow Wall Was Caught In The SWSH DlC, after 55 encounters",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Chedder.jpeg",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/d/d0/Stakataka.png", //From same wiki
         tags: ["Favourite", "Ultra Beast", "Rock", "Steel", "SwSh"]
     },
     {
         nickname: "Moonlight",
         species: "#609 - Chandelure",
         description: "This Flaming Candle Was Caught In Kitakami",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Moonlight.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/b/b3/Chandelure.png", //From same wiki
         tags: ["Favourite", "Fire", "Ghost", "SV"]
     },
     {
         nickname: "Jiggly",
         species: "#0039 - Jigglypuff",
         description: "This puffball was caught in Paldea",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Jiggly.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/f/f7/Jigglypuff.png",  //From same wiki
         tags: ["Fairy", "Normal", "SV"]
     },
     {
         nickname: "Aqua Antis",
         species: "#0055 - Golduck",
         description: "The Infamous Aqua Antis, Found in the SWSH DLC between my favourite shiny and my target",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Aqua%20Antis.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/9/96/Golduck.png", //From same wiki
         tags: ["Water", "SwSh"]
     },
     {
         nickname: "APE",
         species: "#0057 - Primeape",
         description: "This fighter was found in Paldea while looking for a Breloom",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Primape.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/e/e3/Primeape.png", //From same wiki
         tags: ["Fighting", "SV"]
     },
     {
         nickname: "Anger",
         species: "#0057 - Primeape",
         description: "This warrior was found in Paldea while looking for a Breloom, right after APE",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Primape.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/e/e3/Primeape.png", //From same wiki
         tags: ["Fighting", "SV"]
     },
     {
         nickname: "Fury",
         species: "#0058 - Growlithe",
         description: "This hotshot was found in Paldea",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Fury.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/3/34/Growlithe.png", //From same wiki
         tags: ["Fire", "SV"]
     },
     {
         nickname: "Legend",
         species: "#0059 - Arcanine",
         description: "Fury's older brother, also found in Paldea",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Legendary.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/f/f7/Arcanine.png", //From same wiki
         tags: ["Fire", "SV"]
     },
     {
         nickname: "Slow",
         species: "#0079 - Slowpoke",
         description: "This Simple Creature Was Caught In Paldea",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Slow.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/5/56/Slowpoke.png", //From same wiki
         tags: ["Water", "Psychic", "SV"]
     },
     {
         nickname: "Grapé",
         species: "#0080 - Slowbro",
         description: "Grapé is just as slow as Slow, but looks like a grape, caught in Paldea",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Slowbro.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/d/d2/Slowbro.png",  //From same wiki
         tags: ["Water", "Psychic", "SV"]
     },
     {
         nickname: "Clover",
         species: "#0084 - Doduo",
         description: "This sili birb was caught at Blueberry Acadamy",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Clover.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/4/48/Doduo.png", //From same wiki
         tags: ["Normal", "Flying", "SV"]
     },
     {
         nickname: "Dodrio, Formally Lucky",
         species: "#0085 - Dodrio",
         description: "My first caught shiny, caught in PoGo",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Lucky.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/3/37/Dodrio.png",  //From same wiki
         tags: ["Normal", "Flying", "PoGo", "Favourite"]
     },
     {
         nickname: "Mint",
         species: "#0088 - Grimer",
         description: "This suprisingly nice smelling pile of poison was found while hunting Ditto in Paldea",
-        imageUrl: "https://github.com/Rosesandthorns/Rosies-Shinies/blob/main/images/Mint.png",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/5/5f/Grimer.png", //From same wiki
         tags: ["Poison", "SV"]
     },
     {
         nickname: "Rotten",
         species: "#0089 - Muk",
         description: "Mint's rotten cousin, Rotten was found while hunting Ditto in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/2/27/Muk.png",  //From same wiki
         tags: ["Poison", "SV"]
     },
     {
         nickname: "Volté",
         species: "#0100 - Voltorb",
         description: "This shocking orb was found in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/4/41/Voltorb.png", //From same wiki
         tags: ["Electric", "SV"]
     },
     {
         nickname: "Electro",
         species: "#0101 - Electrode",
         description: "This shocking behemith was found in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/6/64/Electrode.png", //From same wiki
         tags: ["Electric", "SV"]
     },
     {
         nickname: "Sap",
         species: "#0103 - Alolan Exeggutor",
         description: "No one ever told sap to keep her head out of the clouds when at blueberry academy",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/0/08/Exeggutor-Alola.png", //From same wiki
         tags: ["Grass", "Dragon", "SV"]
     },
     {
         nickname: "Summer",
         species: "#0103 - Exeggutor",
         description: "Summer loved the sun so much, she tried to reach for it, *almost there*. caught at blueberry academy",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/a/a5/Exeggutor.png",  //From same wiki
         tags: ["Grass", "Dragon", "SV"]
     },
     {
         nickname: "Koffing",
         species: "#0109 - Koffing",
         description: "I caught this over a year ago in Kitakami, No clue why it's not named~",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/3/37/Koffing.png", //From same wiki
         tags: ["Poison", "SV"]
     },
     {
         nickname: "Horseshoe",
         species: "#0128 - Paldean Tauros",
         description: "This raging tauros was for my first playthrough of violet, caught him on the second day of the game's release",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/5/57/Tauros-Paldea-Combat.png", //From same wiki, Combat Breed
         tags: ["Fighting", "Favourite", "SV"]
     },
     {
         nickname: "lavander",
         species: "#0131 - Lapras",
         description: "This lapras was found in the chilly seas of the polar biome in Blueberry Academy",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/3/37/Lapras.png", //From same wiki
         tags: ["Ice", "Water", "SV"]
     },
     {
         nickname: "Bloby",
         species: "#0132 - Ditto",
         description: "This shapeshifter kept me busy on hunting in Paldea for a few days",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/b/b7/Ditto.png", //From same wiki
         tags: ["Normal", "SV"]
     },
     {
         nickname: "Iggly",
         species: "#0174 - Igglybuff",
-        description: "Aww, what a cuti- wait why do you have competitive and copycat-    Found in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        description: "Aww, what a cuti- wait why do you have competitive and copycat-  Found in Paldea",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/b/bf/Igglybuff.png", //From same wiki
         tags: ["Normal", "Fairy", "SV"]
     },
     {
         nickname: "Fluffy",
         species: "#0179 - Mareep",
         description: "Fluffy is what would happen if cotten candy was alive, found in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/9/94/Mareep.png", //From same wiki
         tags: ["Electric", "SV"]
     },
     {
         nickname: "Flaffy",
         species: "#0180 - Flaaffy",
         description: "No, nor the name or species is a typo, thats how Flaaffy is spelt, its weird, caught in paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/f/f5/Flaaffy.png", //From same wiki
         tags: ["Electric", "SV"]
     },
     {
         nickname: "Shine",
         species: "#0183 - Marill",
         description: "this green orb was caught in Paldea, do you think you can use it to awaken Primal Rayquaza?",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/2/21/Marill.png", //From same wiki
         tags: ["Water", "Fairy", "SV"]
     },
     {
         nickname: "Golden",
         species: "#0184 - Azumaril",
         description: "My first boosted odds shiny of Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/f/f9/Azumarill.png", //From same wiki
         tags: ["Water", "Fairy", "SV"]
     },
     {
         nickname: "Sudo",
         species: "#0185 - Sudowoodo",
         description: "This is a natural tree of Paldea, what do you mean its a Pokemon?",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/9/97/Sudowoodo.png", //From same wiki
         tags: ["Rock", "SV"]
     },
     {
         nickname: "Green",
         species: "#0187 - Hoppip",
         description: "This Hoppip is said to have flown over all of Paldea until it got sunburnt, and then turned green",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/5/57/Hoppip.png", //From same wiki
         tags: ["Grass", "Flying", "SV"]
     },
     {
         nickname: "Hoppy",
         species: "#0188 - Skiploom",
         description: "The legend of Green may have been wrong, but you Cant deny the legend of Hoppy getting sunburnt in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/8/8b/Skiploom.png",//From same wiki
         tags: ["Grass", "Flying", "SV"]
     },
     {
         nickname: "Jumpy",
         species: "#0189 - Jumpluff",
         description: "Jumpy is the living embodiment of :3. bringing :3 to Paldea to this day",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/1/18/Jumpluff.png", //From same wiki
         tags: ["Grass", "Flying", "SV"]
     },
     {
         nickname: "Poffy",
         species: "#0194 - Wooper",
         description: "This pink goof has no thoughts at any given time, caught in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/9/93/Wooper.png", //From same wiki
         tags: ["Water", "Ground", "SV"]
     },
     {
         nickname: "King",
         species: "#0199 - Slowking",
         description: "This queen is slaying all gender norms in Paldea, slay Queen!",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/6/60/Slowking.png", //From same wiki
         tags: ["Water", "Psychic", "SV"]
     },
     {
         nickname: "Forret",
         species: "#0205 - Forretress",
         description: "This is how the great crator of Paldea was formed",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/1/12/Forretress.png", //From same wiki
         tags: ["Bug", "Steel", "SV"]
     },
     {
         nickname: "Dun",
         species: "#0206 - Dunsparce",
         description: "This snake was found in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/8/84/Dunsparce.png",//From same wiki
         tags: ["Normal", "SV"]
     },
     {
         nickname: "Present",
-        species: "#0225 - Deibird",
+        species: "#0225 - Delibird",
         description: "Present is almost always late for Christmas in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/5/51/Delibird.png", //From same wiki
         tags: ["Ice", "Flying", "SV"]
     },
     {
         nickname: "Nick",
         species: "#0225 - Delibird",
         description: "Present's twin, caught in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/5/51/Delibird.png",  //From same wiki
         tags: ["Ice", "Flying", "SV"]
     },
     {
         nickname: "Dour",
         species: "#0228 - Houndour",
         description: "This dark puppy was found in a cave in Paldea",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/2/2c/Houndour.png", //From same wiki
         tags: ["Fire", "Dark", "SV"]
     },
     {
         nickname: "Doom",
         species: "#0229 - Houndoom",
         description: "Doom was caught in the depths of Poco Path",
-        imageUrl: "https://via.placeholder.com/200",
+        imageUrl: "https://static.wikia.nocookie.net/shiny-pokemon/images/3/39/Houndoom.png", //From same wiki
         tags: ["Fire", "Dark", "SV"]
     },
     {
