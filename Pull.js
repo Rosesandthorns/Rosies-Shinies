27907f42e896ef2513752c19e38e4cdb8792833e
Rosesandthorns <powwerofpowwer@gmail.com>
2025-06-10 10:41:58 -0400

    Create Pull.js

---
+++ Pull.js
@@ -0,0 +1,104 @@
+// data.js
+const pokemonList = [
+    {
+      nickname: "Chop",
+      species: "#475 - Gallade",
+      description: "My probably most used pokemon ever, Chop is my false swiper and helps me shiny hunt",
+      imageUrl: "https://www.serebii.net/Shiny/SWSH/475.png",
+      tags: ["Favourite", "Fighting", "Psychic", "SV"],
+      level: 100,
+      nature: "Adamant",
+      moveset: ["False Swipe", "Heal Pulse", "Hypnosis", "Psychic"]
+    },
+    {
+      nickname: "Syrup",
+      species: "#1011 - Dipplin",
+      description: "Placeholder",
+      imageUrl: "",
+      tags: ["Grass", "Dragon", "Favourite", "SV"],
+      level: 100,
+      nature: "Mild",
+      moveset: ["Astonish", "Withdraw", "Energy Ball", "Drago Pulse"]
+    },
+    {
+      nickname: "Eclipse",
+      species: "#0792 - Lunala",
+      description: "Placeholder",
+      imageUrl: "",
+      tags: ["Psychic", "Ghost", "Legendary", "Favourite" "SwSh"],
+      level: 100,
+      nature: "Quiet",
+      moveset: ["Moonblast", "Phantom Force", "Dream Eater", "Wide Guard"]
+    },
+    {
+      nickname: "Volt",
+      species: "#0738 - Vikavolt",
+      description: "Placeholder",
+      imageUrl: "",
+      tags: ["Bug", "Electric", "SV"],
+      level: 100,
+      nature: "Careful",
+      moveset: ["Bug", "Electric", "Favourite", "SV"]
+    },
+    {
+      nickname: "Token",
+      species: "#0706 - Goodra",
+      description: "Placeholder",
+      imageUrl: "",
+      tags: ["Dragon", "Favourite", "SV"],
+      level: 100,
+      nature: "Sassy",
+      moveset: ["Acid Spray", "Power Whip", "Aqua Tail", "Rain Dance"]
+    },
+    {
+      nickname: "Golden",
+      species: "#0184 - Azumaril",
+      description: "Placeholder",
+      imageUrl: "",
+      tags: ["Water", "Fairy", "SV"],
+      level: 99,
+      nature: "Naughty",
+      moveset: ["Charm", "Slam", "Bounce", "Aqua Tail"]
+    },
+    {
+      nickname: "MoonLight",
+      species: "#0609 - Chandelure",
+      description: "This was my first hunt after a 6 month break",
+      imageUrl: "",
+      tags: ["Fire", "Ghost", "Favourite", "SV"],
+      level: 97,
+      nature: "Jolly",
+      moveset: ["Confuse Ray", "Hex", "Will-O-Wisp", "Overheat"]
+    },
+    {
+      nickname: "Snowflake",
+      species: "#0474 - Porygon-Z",
+      description: "Placeholder",
+      imageUrl: "",
+      tags: ["Normal", "SV"],
+      level: 89,
+      nature: "Quirky",
+      moveset: ["Tri Attack", "Double-Edge", "Lock-On", "Zap Cannon"]
+    },
+    {
+      nickname: "Golden",
+      species: "#0250 - Ho-Oh",
+      description: "Placeholder",
+      imageUrl: "",
+      tags: ["Fire", "Flying", "Legendary," "Favourite", "SwSh"],
+      level: 88,
+      nature: "Lonely",
+      moveset: ["Extrasensory", "Recover", "Sacred Fire", "Sunny Day"]
+    },
+    {
+      nickname: "Lavander",
+      species: "#0131 - Lapras",
+      description: "Placeholder",
+      imageUrl: "",
+      tags: ["Water", "Ice", "SV"],
+      level: 84,
+      nature: "Hardy",
+      moveset: ["Confuse Ray", "Hydro Pump", "Perish Song", "Sheer Cold"]
+    },
+    ];
+const originalPokemonList = [...pokemonList]; // Store original list
